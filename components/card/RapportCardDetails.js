2c86dca6e44d24e2cc1a21c098ca35374cd25a21
matthieup-git <matthieu.posnic@gmail.com>
2025-03-21 20:52:21 +0100

    push

--- components/card/RapportCardDetails.js
+++ components/card/RapportCardDetails.js
@@ -24,7 +24,7 @@ function RapportCardDetails({ onModifyChange }) {
         }
     };
 
-    const displayDescription = description.replace(/\n/g, '<br>');
+    const displayDescription = description ? description?.replace(/\n/g, '<br>') : ""
 
     return (
         <div className="flex flex-col gap-4">
