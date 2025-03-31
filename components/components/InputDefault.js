193dea2156a431b181e1d4a2d87bb19a2e24a140
matthieup-git <matthieu.posnic@gmail.com>
2025-03-31 17:37:19 +0200

    autocapitalize ok

--- components/components/InputDefault.js
+++ components/components/InputDefault.js
@@ -5,11 +5,12 @@ import { Input } from "../../src/components/components/ui/input";
 import { useRouter } from "next/router";
 
 function InputDefault({ type, placeholder = "", onChange, value, className }) {
+
   const router = useRouter();
 
   className === "error" ? (className = "bg-input-bg-error border-input-stroke-error") : "";
 
-  router.pathname === "/" ? (autoCapitalize = autoCapitalize = "none") : "on";
+  const autoCapitalizeValue = router.pathname === "/" ? "none" : "on";
 
   return <Input type={type} placeholder={placeholder} onChange={onChange} value={value} className={className} autoCapitalize={autoCapitalizeValue}/>;
 }
